615fea37a9be63b05d53d73322ec1fadb5449027
toby <smorgrav@yahoo-inc.com>
2017-08-31 13:22:15 +0200

    More quickstart

--- quickstart.sh
+++ quickstart.sh
@@ -16,4 +16,15 @@ if [ ! "$(docker ps -q -f name=vespa)" ]; then
 	docker run --detach --name vespa --hostname vespa-container --privileged --volume $PWD/sample-apps:/vespa-sample-apps --publish 8080:8080 vespaengine/vespa
 fi
 
+echo "Waiting for Vespa config to start up"
+sleep 5
+
 docker exec vespa sh -c "/opt/vespa/bin/vespa-deploy prepare /vespa-sample-apps/basic-search/src/main/application && /opt/vespa/bin/vespa-deploy activate"
+
+echo "Waiting for the application to be ready..."
+until $(curl --output /dev/null --fail -s --head http://localhost:8080/ApplicationStatus); do
+    printf '.'
+    sleep 5
+done
+
+echo "Vespa is ready!"
